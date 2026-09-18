c7a811119a2ac9045560652878e87f1a7fc29ba9
gizmore <gizmore@wechall.net>
2026-09-18 04:05:43 +0200

    'more korean'

---
+++ lang/logs_ko.php
@@ -0,0 +1,20 @@
+<?php
+namespace GDO\Logs\lang;
+
+return [
+	'module_logs' => '로그',
+	'mt_logs_overview' => '로그',
+	'logs_overview' => '내 로그',
+	'cfg_log_keep_for_time' => '다음에 대한 로그 파일 보관',
+	'cfg_log_by_mail' => '이메일로 순환 로그 보내기',
+	'cfg_log_mail_to' => '로그 아카이브 수신자',
+	'cfg_log_delete_after_mail' => '이메일 전송 성공 후 로컬 아카이브 삭제',
+	'logs_no_files' => '사용 가능한 로그 파일이 없습니다.',
+	'logs_select_file' => '로그 파일을 선택하여 확인하세요.',
+	'logs_search' => '로그 검색',
+	'logs_filter_files' => '파일 필터링',
+	'logs_search_result' => '%s에 대한 검색 결과',
+	'logs_no_matches' => '일치하는 로그 항목이 없습니다.',
+	'mail_subj_logs_archive' => '%s: 로그 아카이브',
+	'mail_body_logs_archive' => '%s에 대해 새로 순환된 로그 아카이브가 첨부되어 있습니다.',
+];
